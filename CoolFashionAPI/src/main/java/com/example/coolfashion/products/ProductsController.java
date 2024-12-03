17bd8a5c66e57b2d53839cfda970198230e849af
MohammedMohammed2 <151514374+MohammedMohammed2@users.noreply.github.com>
2024-12-03 16:15:28 +0100

    comit

--- CoolFashionAPI/src/main/java/com/example/coolfashion/products/ProductsController.java
+++ CoolFashionAPI/src/main/java/com/example/coolfashion/products/ProductsController.java
@@ -1,6 +1,10 @@
 package com.example.coolfashion.products;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +13,9 @@ public class ProductsController {
     ProductsService productsService;
 
 
+    @PostMapping("/createProduct/{title}")
+    public ResponseEntity<ProductsModel> createProduct(@RequestBody ProductsModel product) {
+        ProductsModel createdProduct = productsService.createProduct(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdProduct);
+    }
 }
--- CoolFashionAPI/src/main/java/com/example/coolfashion/products/ProductsService.java
+++ CoolFashionAPI/src/main/java/com/example/coolfashion/products/ProductsService.java
@@ -3,8 +3,20 @@ package com.example.coolfashion.products;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ProductsService {
     @Autowired
     ProductsRepository productsRepository;
+
+    @Autowired
+    private ProductsRepository productRepository;
+
+
+
+    public ProductsModel createProduct(ProductsModel product) {
+        return productRepository.save(product);
+    }
 }
+
