ff5b9ddc802a15a32a0281b3ac55c842a9318e1f
MohammedMohammed2 <151514374+MohammedMohammed2@users.noreply.github.com>
2024-12-04 18:13:50 +0100

    reverted again lol

--- CoolFashionAPI/src/main/java/com/example/coolfashion/products/ProductsController.java
+++ CoolFashionAPI/src/main/java/com/example/coolfashion/products/ProductsController.java
@@ -3,18 +3,33 @@ package com.example.coolfashion.products;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+<<<<<<< HEAD
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.List;
+=======
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+>>>>>>> parent of 8adb48f (reverted)
 
-@RestController
+@Controller
 public class ProductsController {
     @Autowired
     ProductsService productsService;
 
+<<<<<<< HEAD
     @RestController
     public class MyController {
+=======
+    @GetMapping("/getProductForm")
+    public String getProductForm(){
+        return "create_new_product_in_category";
+    }
+>>>>>>> parent of 8adb48f (reverted)
 
         // Allow CORS for this specific method
         @GetMapping("/my-endpoint")
@@ -23,6 +38,15 @@ public class ProductsController {
             return Collections.singletonList(productsService.getProduct());
         }
     }
+<<<<<<< HEAD
+=======
+
+    @GetMapping("/contactUs.html")
+    public String getContactForm(){
+        return "contactUs";
+    }
+
+>>>>>>> parent of 8adb48f (reverted)
     @PostMapping("/create_product")
     public ResponseEntity<ProductsModel> createProduct(@RequestBody ProductsModel product) {
         ProductsModel createdProduct = productsService.createProduct(product);
