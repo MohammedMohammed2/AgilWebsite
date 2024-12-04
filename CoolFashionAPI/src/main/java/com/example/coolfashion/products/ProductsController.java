6d547263a71cfa437265fc9bf19ec9134891d508
MohammedMohammed2 <151514374+MohammedMohammed2@users.noreply.github.com>
2024-12-04 14:19:33 +0100

    moved all the js files html and css to templates and static folders

--- CoolFashionAPI/src/main/java/com/example/coolfashion/products/ProductsController.java
+++ CoolFashionAPI/src/main/java/com/example/coolfashion/products/ProductsController.java
@@ -3,15 +3,22 @@ package com.example.coolfashion.products;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class ProductsController {
     @Autowired
     ProductsService productsService;
 
+
+    @GetMapping("/getProductForm")
+    public String getProductForm(){
+        return "create_new_product_in_category";
+    }
 
     @PostMapping("/create_product")
     public ResponseEntity<ProductsModel> createProduct(@RequestBody ProductsModel product) {
