b9faca0551a635a021aebfd914fea706ed18c19e
Snozzyy <oliverschuller@hotmail.com>
2024-12-04 18:42:58 +0100

    Fixed mapping

---
+++ CoolFashionAPI/src/main/java/com/example/coolfashion/Mapping.java
@@ -0,0 +1,64 @@
+package com.example.coolfashion;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Mapping {
+
+    @GetMapping("/index.html")
+    public String getHomePage(){
+        return "index";
+    }
+
+    @GetMapping("/contactUs.html")
+    public String getContactForm(){
+        return "contactUs";
+    }
+
+    @GetMapping("/getProductForm.html")
+    public String getProductForm(){
+        return "create_new_product_in_category";
+    }
+
+    @GetMapping("/secondHand.html")
+    public String getSecondHand() {
+        return "secondHand";
+    }
+
+    @GetMapping("added_products.html")
+    public String getAddedProduct() {
+        return "added_products";
+    }
+
+    @GetMapping("admin_frontpage.html")
+    public String getAdminFrontpage() {
+        return "admin_frontpage";
+    }
+
+    @GetMapping("adminAddCategory.html")
+    public String getAdminAddCategory() {
+        return "adminAddCategory";
+    }
+
+    @GetMapping("create_new_product_in_category.html")
+    public String getCreateNewProductInCategory() {
+        return "create_new_product_in_category";
+    }
+
+    @GetMapping("create_product.html")
+    public String createProduct() {
+        return "create_product";
+    }
+
+    @GetMapping("edit_product.html")
+    public String editProduct() {
+        return "edit_product";
+    }
+
+    @GetMapping("login.html")
+    public String login() {
+        return "login";
+    }
+
+}
