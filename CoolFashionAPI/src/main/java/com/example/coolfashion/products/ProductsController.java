dea504701d186a48c2ec2fefe9af20ed2f6e7ed3
MohammedMohammed2 <151514374+MohammedMohammed2@users.noreply.github.com>
2024-12-04 21:30:07 +0100

    fixed create products

--- CoolFashionAPI/src/main/java/com/example/coolfashion/products/ProductsController.java
+++ CoolFashionAPI/src/main/java/com/example/coolfashion/products/ProductsController.java
@@ -28,6 +28,12 @@ public class ProductsController {
         return Collections.singletonList(productsService.getProduct());
     }
 
+
+    @GetMapping("/getProductForm")
+    public String getProductForm(){
+        return "create_new_product_in_category";
+    }
+
     @PostMapping("/create_product")
     public ResponseEntity<ProductsModel> createProduct(@RequestBody ProductsModel product) {
         ProductsModel createdProduct = productsService.createProduct(product);
