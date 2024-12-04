46e637e2c0e658a1272771af1c5c9720035af7b3
MohammedMohammed2 <151514374+MohammedMohammed2@users.noreply.github.com>
2024-12-04 18:17:22 +0100

    deleted some things that were there bc of the reverting

--- CoolFashionAPI/src/main/java/com/example/coolfashion/products/ProductsController.java
+++ CoolFashionAPI/src/main/java/com/example/coolfashion/products/ProductsController.java
@@ -3,50 +3,39 @@ package com.example.coolfashion.products;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-<<<<<<< HEAD
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.List;
-=======
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
->>>>>>> parent of 8adb48f (reverted)
+
 
 @Controller
 public class ProductsController {
     @Autowired
     ProductsService productsService;
 
-<<<<<<< HEAD
-    @RestController
-    public class MyController {
-=======
     @GetMapping("/getProductForm")
     public String getProductForm(){
         return "create_new_product_in_category";
     }
->>>>>>> parent of 8adb48f (reverted)
-
-        // Allow CORS for this specific method
-        @GetMapping("/my-endpoint")
-        @CrossOrigin(origins = "https://localhost:8080", allowedHeaders = "Content-Type, Authorization", methods = {RequestMethod.GET})
-        public List<Object> getData() {
-            return Collections.singletonList(productsService.getProduct());
-        }
+    // Allow CORS for this specific method
+    @GetMapping("/my-endpoint")
+    @CrossOrigin(origins = "https://localhost:8080", allowedHeaders = "Content-Type, Authorization", methods = {RequestMethod.GET})
+    public List<Object> getData() {
+        return Collections.singletonList(productsService.getProduct());
     }
-<<<<<<< HEAD
-=======
-
     @GetMapping("/contactUs.html")
     public String getContactForm(){
         return "contactUs";
     }
 
->>>>>>> parent of 8adb48f (reverted)
     @PostMapping("/create_product")
     public ResponseEntity<ProductsModel> createProduct(@RequestBody ProductsModel product) {
         ProductsModel createdProduct = productsService.createProduct(product);
